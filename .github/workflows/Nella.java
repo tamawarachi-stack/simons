17080f67d1b1572ef03d70a4ce7efad0481fee90
tamawarachi-stack <tamawarachi@gmail.com>
2026-02-21 04:53:07 +0700

    Create Nella.java

---
+++ .github/workflows/Nella.java
@@ -0,0 +1,17 @@
+package com.nella.app;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.Gravity;
+
+public class Nella extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("เชื่อมต่อสำเร็จ! \n\n บอกแนวแอปที่คุณต้องการมาได้เลย");
+        tv.setTextSize(20);
+        tv.setGravity(Gravity.CENTER);
+        setContentView(tv);
+    }
+}
